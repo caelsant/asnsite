466650e2eff372acd263c2e88ed42818fc83dd9e
Cael <micael.santana233@gmail.com>
2022-03-21 03:05:54 -0300

    Deletando Node

---
+++ aplicacao.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var app = express();
+const path = require("path")
+
+app.use(express.static('public'))
+app.use(express.static('css'))
+
+app.get("/index", function(req, res){
+    res.sendFile(path.join(__dirname + "/index.html"))
+})
+
+
+
+app.listen(9090, function(){
+    console.log("Rodando Normalmente")
+});
